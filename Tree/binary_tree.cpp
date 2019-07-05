043a34cdc71dd3b344cdef0d0adf4181ad376095
Bharat Arya <arya.bharat@rocketmail.com>
2019-07-05 21:18:55 +0530

    Update binary_tree.cpp

--- Tree/binary_tree.cpp
+++ Tree/binary_tree.cpp
@@ -1,17 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 struct node
 {
-  node * left;
-  node * right;
-  int data;
+    int data;
+    node *left;
+    node *right;
 };
-
+node *root = NULL;
+node *getNewNode(int data)
+{
+    node *temp = new node();
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+void insert(node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = getNewNode(data);
+    }
+    else if (data >= root->data)
+    {
+        insert(root->right, data);
+    }
+    else
+    {
+        insert(root->left,data);
+    }
+}
 int main()
 {
-  node * root = new node;
-
-  return 0;
-
+    insert(root, 10);
+    return 0;
 }
